0f0734e8317f0e644694f4d9ea6a87fad6748654
Twizzy0127 <143265931+Twizzy0127@users.noreply.github.com>
2024-06-13 22:33:34 +0800

    Create variable.js

---
+++ variable.js
@@ -0,0 +1,48 @@
+// Declare a variable named 'carName', assign the value 'Volvo' to it.
+// carName гэсэн хувьсагч зарлаад Volvo гэсэн утга онооно уу .
+
+// Declare a variable named 'name' , assign it your name.
+// name гэсэн хувьсагч зарлаад өөрийнхөө нэрийг онооно уу .
+
+// Declare a variable 'futureJob' without assigning a value.
+// futureJob гэсэн хувьсагчийг утга оноохгүйгээр зарлана уу .
+
+// Declare a variable 'age', assign it your name.
+// 'age' гэсэн хувьсагч зарлаад өөрийн насыг онооно уу .
+
+// Declare a variable 'PI' and assign it the value 3.14.
+// PI гэсэн тогтмол утга зарлаад 3.14 гэсэн утга онооно уу .
+
+// Display the sum of 5 + 10, using two variables: x and y.
+// x ба y гэсэн хоёр хувьсагчийг ашиглан 5 + 10-ын нийлбэрийг харуулна уу .
+
+// Declare three variables for each let , const. 
+// for example : const motherName = 'Chimgee' ; let presidentName = 'Khurelsukh' 
+// let , const тус бүрт нь 3 хувьсагч зарлана уу .
+
+// Declare a variable 'city' and assign it a city name, then reassign it to another city.
+// city гэсэн хувьсагч зарлаад хотын нэр онооно уу , дараа нь ахин өөр хотын нэр онооно уу .
+
+// Declare variables 'firstName' and 'lastName', then concatenate them into a variable 'fullName'.
+// firstName , lastName гэсэн хувьсагчууд зарлаад , дараа нь нэгтгэн fullName гэсэн хувьсагчид онооно уу .
+
+// Declare a variable 'isStudent' and assign it a boolean value.
+// isStudent гэсэн хувьсагч зарлаад boolean утга онооно уу .
+
+// Declare a variable 'isRaining' and assign it a boolean value.
+// isRaining гэсэн хувьсагч зарлаад boolean утга онооно уу .
+
+// Declare a string value.
+// String утга зарлана уу .
+
+// Declare a number value.
+// Number утга зарлана уу .
+
+// Declare an undefined value.
+// Undefined утга зарлана уу .
+
+// Declare a null value.
+// Null утга зарлана уу .
+
+// Declare a boolean value.
+// Boolean утга зарлана уу .
